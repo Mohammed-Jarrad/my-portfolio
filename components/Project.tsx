34f6de05fda2e1009a1d81330fadd7b89c392637
MohammedJarrad <mohammed.w.jarrad@gmail.com>
2024-06-14 18:55:11 +0300

    add another projects

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,130 +1,137 @@
-import { projectsData } from '@/lib/data'
-import { motion, useScroll, useTransform } from 'framer-motion'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRef } from 'react'
-import { AiFillGithub } from 'react-icons/ai'
-import { BsGlobe } from 'react-icons/bs'
-import { useInView } from 'react-intersection-observer'
-
-type ProjectProps = (typeof projectsData)[number] & { index: number }
+import type { Project } from "@/lib/data";
+import { motion, useScroll, useTransform } from "framer-motion";
+import Image from "next/image";
+import Link from "next/link";
+import { useRef } from "react";
+import { AiFillGithub } from "react-icons/ai";
+import { BsGlobe } from "react-icons/bs";
+import { useInView } from "react-intersection-observer";
 
 export function Project({
-	title,
-	description,
-	tags,
-	image,
-	imgLink,
-	github,
-	githubBackend,
-	mainColor,
-	index,
-}: ProjectProps) {
-	const articleRef = useRef<HTMLDivElement>(null)
-	const { ref: contentRef, inView } = useInView({ triggerOnce: false, threshold: 0.3 })
-	const imageVarients = {
-		hidden: (index + 1) % 2 == 0 ? { right: '100%', opacity: 0 } : { left: '100%', opacity: 0 },
-		visible: (index + 1) % 2 == 0 ? { right: '50%', opacity: 1 } : { left: '50%', opacity: 1 },
-	}
-	const headerVarients = {
-		hidden: { y: -100, opacity: 0 },
-		visible: { y: 0, opacity: 1 },
-	}
-	const elementsVarients = {
-		hidden: (index + 1) % 2 == 0 ? { x: 100, opacity: 0 } : { x: -100, opacity: 0 },
-		visible: { x: 0, opacity: 1 },
-	}
-	const linkItemsVarients = {
-		hidden: { x: 100, opacity: 0 },
-		visible: { x: 0, opacity: 1 },
-	}
-	const { scrollYProgress } = useScroll({
-		target: articleRef,
-		offset: ['0 1', '1.1 1'],
-	})
-	const newScale = useTransform(scrollYProgress, [0, 1], [0.7, 1])
-	const newOpacity = useTransform(scrollYProgress, [0, 1], [0.5, 1])
+    title,
+    description,
+    tags,
+    image,
+    imgLink,
+    github,
+    githubBackend,
+    mainColor,
+    index,
+}: Project & { index: number }) {
+    const articleRef = useRef<HTMLDivElement>(null);
+    const { ref: contentRef, inView } = useInView({ triggerOnce: false, threshold: 0.3 });
+    const imageVarients = {
+        hidden: (index + 1) % 2 == 0 ? { right: "100%", opacity: 0 } : { left: "100%", opacity: 0 },
+        visible: (index + 1) % 2 == 0 ? { right: "50%", opacity: 1 } : { left: "50%", opacity: 1 },
+    };
+    const headerVarients = {
+        hidden: { y: -100, opacity: 0 },
+        visible: { y: 0, opacity: 1 },
+    };
+    const elementsVarients = {
+        hidden: (index + 1) % 2 == 0 ? { x: 100, opacity: 0 } : { x: -100, opacity: 0 },
+        visible: { x: 0, opacity: 1 },
+    };
+    const linkItemsVarients = {
+        hidden: { x: 100, opacity: 0 },
+        visible: { x: 0, opacity: 1 },
+    };
+    const { scrollYProgress } = useScroll({
+        target: articleRef,
+        offset: ["0 1", "1.1 1"],
+    });
+    const newScale = useTransform(scrollYProgress, [0, 1], [0.7, 1]);
+    const newOpacity = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
 
-	return (
-		<motion.article
-			className="group relative overflow-hidden mb-4 md:mb-8 last:mb-0 border-2 border-black/5 rounded-lg md:min-h-[20rem]  bg-gray-100 hover:bg-gray-200 transition max-sm:min-h-[65vh] max-md:flex max-md:flex-col max-md:justify-between dark:bg-[#FFFFFF0D] dark:border-gray-600 dark:border-1 dark:border-opacity-10"
-			style={{ scale: newScale, opacity: newOpacity }}
-			ref={articleRef}
-		>
-			<div
-				ref={contentRef}
-				className="p-4 max-w-[50%] flex flex-col h-full max-md:max-w-full group-even:ml-auto max-md:flex-1 space-y-6"
-			>
-				<motion.h3
-					variants={headerVarients}
-					initial={'hidden'}
-					animate={inView ? 'visible' : 'hidden'}
-					transition={{ duration: 0.5 }}
-					className="font-semibold font-comfortaa mb-3 text-lg"
-				>
-					{title}
-				</motion.h3>
-				<motion.p
-					variants={elementsVarients}
-					initial={'hidden'}
-					animate={inView ? 'visible' : 'hidden'}
-					transition={{ duration: 0.2 }}
-					className="text-gray-700 font-comfortaa text-sm max-md:mb-4 dark:text-gray-300"
-				>
-					{description}
-				</motion.p>
-				<ul className="flex items-center flex-wrap gap-3 mt-auto">
-					{[...tags].map((tag, idx) => (
-						<motion.li
-							key={tag}
-							className="bg-black/60 text-white rounded-full py-1 px-3 text-center text-[13px] capitalize font-comfortaa dark:bg-white/10"
-							variants={linkItemsVarients}
-							initial={'hidden'}
-							animate={inView ? 'visible' : 'hidden'}
-							transition={{ duration: 0.3, delay: 0.18 * (idx + 1) }}
-						>
-							{tag}
-						</motion.li>
-					))}
-				</ul>
-				<motion.div
-					variants={elementsVarients}
-					initial={'hidden'}
-					animate={inView ? 'visible' : 'hidden'}
-					transition={{ duration: 0.5 }}
-					className="flex items-center mt-4 gap-3"
-				>
-					<Link
-						target="_blank"
-						href={imgLink}
-						className="project_button text-white border-transparent shadow-lg"
-						style={{ background: mainColor }}
-					>
-						Visit <BsGlobe />
-					</Link>
-					<Link href={github} target="_blank" className="project_button shadow-lg">
-						{!githubBackend ? 'Source Code' : 'Frontend'} <AiFillGithub className="text-[16px]" />
-					</Link>
-					{githubBackend && (
-						<Link href={githubBackend} target="_blank" className="project_button shadow-lg">
-							Backend <AiFillGithub className="text-[16px]" />
-						</Link>
-					)}
-				</motion.div>
-			</div>
+    return (
+        <motion.article
+            className="group relative overflow-hidden mb-4 md:mb-8 last:mb-0 border-2 border-black/5 rounded-lg md:min-h-[20rem]  bg-gray-100 hover:bg-gray-200 transition max-sm:min-h-[65vh] max-md:flex max-md:flex-col max-md:justify-between dark:bg-[#FFFFFF0D] dark:border-gray-600 dark:border-1 dark:border-opacity-10"
+            style={{ scale: newScale, opacity: newOpacity }}
+            ref={articleRef}
+        >
+            <div
+                ref={contentRef}
+                className="p-4 max-w-[50%] flex flex-col h-full max-md:max-w-full group-even:ml-auto max-md:flex-1 space-y-6"
+            >
+                <motion.h3
+                    variants={headerVarients}
+                    initial={"hidden"}
+                    animate={inView ? "visible" : "hidden"}
+                    transition={{ duration: 0.5 }}
+                    className="font-semibold font-comfortaa mb-3 text-lg"
+                >
+                    {title}
+                </motion.h3>
+                <motion.p
+                    variants={elementsVarients}
+                    initial={"hidden"}
+                    animate={inView ? "visible" : "hidden"}
+                    transition={{ duration: 0.2 }}
+                    className="text-gray-700 font-comfortaa text-sm max-md:mb-4 dark:text-gray-300"
+                >
+                    {description}
+                </motion.p>
+                <ul className="flex items-center flex-wrap gap-3 mt-auto">
+                    {[...tags].map((tag, idx) => (
+                        <motion.li
+                            key={tag}
+                            className="bg-black/60 text-white rounded-full py-1 px-3 text-center text-[13px] capitalize font-comfortaa dark:bg-white/10"
+                            variants={linkItemsVarients}
+                            initial={"hidden"}
+                            animate={inView ? "visible" : "hidden"}
+                            transition={{ duration: 0.3, delay: 0.18 * (idx + 1) }}
+                        >
+                            {tag}
+                        </motion.li>
+                    ))}
+                </ul>
+                <motion.div
+                    variants={elementsVarients}
+                    initial={"hidden"}
+                    animate={inView ? "visible" : "hidden"}
+                    transition={{ duration: 0.5 }}
+                    className="flex items-center mt-4 gap-3"
+                >
+                    {imgLink && (
+                        <Link
+                            target="_blank"
+                            href={imgLink}
+                            className="project_button text-white border-transparent shadow-lg"
+                            style={{ background: mainColor }}
+                        >
+                            Visit <BsGlobe />
+                        </Link>
+                    )}
+                    {github && (
+                        <Link href={github} target="_blank" className="project_button shadow-lg">
+                            {!githubBackend ? "Source Code" : "Frontend"}{" "}
+                            <AiFillGithub className="text-[16px]" />
+                        </Link>
+                    )}
+                    {githubBackend && (
+                        <Link
+                            href={githubBackend}
+                            target="_blank"
+                            className="project_button shadow-lg"
+                        >
+                            Backend <AiFillGithub className="text-[16px]" />
+                        </Link>
+                    )}
+                </motion.div>
+            </div>
 
-			<motion.div
-				className="absolute top-16 w-[95%] mx-auto max-md:mb-2 md:w-[80%] max-md:static"
-				variants={imageVarients}
-				initial="hidden"
-				animate={inView ? 'visible' : 'hidden'}
-				transition={{ duration: 0.6 }}
-			>
-				<Image
-					src={image}
-					alt={`${title} Project Image`}
-					quality={95}
-					className={`
+            <motion.div
+                className="absolute top-16 w-[95%] mx-auto max-md:mb-2 md:w-[80%] max-md:static"
+                variants={imageVarients}
+                initial="hidden"
+                animate={inView ? "visible" : "hidden"}
+                transition={{ duration: 0.6 }}
+            >
+                <Image
+                    src={image}
+                    alt={`${title} Project Image`}
+                    quality={95}
+                    className={`
                     rounded-lg shadow-2xl transition 
                     md:group-hover:-translate-x-1
                     md:group-hover:translate-y-3
@@ -133,8 +140,8 @@ export function Project({
                     md:group-hover:group-even:translate-x-1
                     md:group-hover:group-even:translate-y-3
                     `}
-				/>
-			</motion.div>
-		</motion.article>
-	)
+                />
+            </motion.div>
+        </motion.article>
+    );
 }
--- lib/data.ts
+++ lib/data.ts
@@ -1,198 +1,273 @@
-import jarradBlogs from '@/public/sites-images/jarrad-blogs.png'
-import jarradDelta from '@/public/sites-images/jarrad-delta.png'
-import jarradMarbleNet from '@/public/sites-images/jarrad-marble-net.png'
-import jarradMercyPharm from '@/public/sites-images/jarrad-mercypharm.png'
-import jarradNike from '@/public/sites-images/jarrad-nike.png'
-import jarradSocialMedia from '@/public/sites-images/jarrad-socialmedia.png'
-import React from 'react'
-import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaReact, FaWhatsapp } from 'react-icons/fa'
-import { LuGraduationCap } from 'react-icons/lu'
+import jarradBlogs from "@/public/sites-images/jarrad-blogs.png";
+import jarradDelta from "@/public/sites-images/jarrad-delta.png";
+import jarradMarbleNet from "@/public/sites-images/jarrad-marble-net.png";
+import jarradMercyPharm from "@/public/sites-images/jarrad-mercypharm.png";
+import jarradNike from "@/public/sites-images/jarrad-nike.png";
+import jarradSocialMedia from "@/public/sites-images/jarrad-socialmedia.png";
+import memobookImage from "@/public/sites-images/memobook.png";
+import mernEcommerceImage from "@/public/sites-images/ecommerce.png";
+import React from "react";
+import { FaFacebook, FaGithub, FaInstagram, FaLinkedin, FaReact, FaWhatsapp } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import { MdVilla } from "react-icons/md";
+import { StaticImageData } from "next/image";
 
 export const links = [
-	{
-		name: 'Home',
-		hash: '#home',
-	},
-	{
-		name: 'About',
-		hash: '#about',
-	},
-	{
-		name: 'Projects',
-		hash: '#projects',
-	},
-	{
-		name: 'Skills',
-		hash: '#skills',
-	},
-	{
-		name: 'Experience',
-		hash: '#experience',
-	},
-	{
-		name: 'Contact',
-		hash: '#contact',
-	},
-] as const
+    {
+        name: "Home",
+        hash: "#home",
+    },
+    {
+        name: "About",
+        hash: "#about",
+    },
+    {
+        name: "Projects",
+        hash: "#projects",
+    },
+    {
+        name: "Skills",
+        hash: "#skills",
+    },
+    {
+        name: "Experience",
+        hash: "#experience",
+    },
+    {
+        name: "Contact",
+        hash: "#contact",
+    },
+] as const;
 
 export const socialMedia = [
-	{
-		name: 'LinkedIn',
-		href: 'https://www.linkedin.com/in/mohammed-jarrad',
-		icon: FaLinkedin,
-	},
-	{
-		name: 'GitHub',
-		href: 'https://github.com/Mohammed-Jarrad',
-		icon: FaGithub,
-	},
-	{
-		name: 'Facebook',
-		href: 'https://www.facebook.com/jarrad.1999',
-		icon: FaFacebook,
-	},
-	{
-		name: 'Instagram',
-		href: 'https://www.instagram.com/mohammed_w_jarrad',
-		icon: FaInstagram,
-	},
-	{
-		name: 'WhatsApp',
-		href: 'https://wa.me/+972568558874',
-		icon: FaWhatsapp,
-	},
-]
+    {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com/in/mohammed-jarrad",
+        icon: FaLinkedin,
+    },
+    {
+        name: "GitHub",
+        href: "https://github.com/Mohammed-Jarrad",
+        icon: FaGithub,
+    },
+    {
+        name: "Facebook",
+        href: "https://www.facebook.com/jarrad.1999",
+        icon: FaFacebook,
+    },
+    {
+        name: "Instagram",
+        href: "https://www.instagram.com/mohammed_w_jarrad",
+        icon: FaInstagram,
+    },
+    {
+        name: "WhatsApp",
+        href: "https://wa.me/+972568558874",
+        icon: FaWhatsapp,
+    },
+];
 
 export const personalDetails = {
-	linkdIn: {
-		name: 'LinkedIn',
-		href: 'https://www.linkedin.com/in/mohammed-jarrad',
-		icon: React.createElement(FaLinkedin),
-	},
-	github: {
-		name: 'GitHub',
-		href: 'https://github.com/Mohammed-Jarrad',
-		icon: React.createElement(FaGithub),
-	},
-	cv: {
-		location: '/Mohammed Jarrad Resume.pdf',
-	},
-	personalImg: {
-		src: '/pimage1.png',
-		alt: 'Mohammed Jarrad Image',
-	},
-	phoneNumber: '+972 56-855-8874',
-	location: 'Palestine - Tulkarem',
-	email: 'mohammed.w.jarrad@gmail.com',
-}
+    linkdIn: {
+        name: "LinkedIn",
+        href: "https://www.linkedin.com/in/mohammed-jarrad",
+        icon: React.createElement(FaLinkedin),
+    },
+    github: {
+        name: "GitHub",
+        href: "https://github.com/Mohammed-Jarrad",
+        icon: React.createElement(FaGithub),
+    },
+    cv: {
+        location: "/Mohammed Jarrad Resume.pdf",
+    },
+    personalImg: {
+        src: "/pimage1.png",
+        alt: "Mohammed Jarrad Image",
+    },
+    phoneNumber: "+972 56-855-8874",
+    location: "Palestine - Tulkarem",
+    email: "mohammed.w.jarrad@gmail.com",
+};
 
 export const experiencesData = [
-	{
-		title: 'Graduated bootcamp',
-		location: 'Palestine, Tulkarem.',
-		description:
-			'I graduated from Computer Systems Engineering department after 5 years of studying where i did the Full MERN Stack graduation project without any partner.',
-		icon: React.createElement(LuGraduationCap),
-		date: 'June, 2022',
-	},
-	{
-		title: 'Full-Stack Developer',
-		location: 'Palestine, Tulkarem.',
-		description:
-			"I'm now a full-stack (MERN) developer working as a freelancer. My stack includes React, Next.js, Node.js, TypeScript, Tailwind, MongoDB. I'm open to full-time opportunities.",
-		icon: React.createElement(FaReact),
-		date: '2022 - present',
-	},
-] as const
+    {
+        title: "Graduated bootcamp",
+        location: "Palestine, Tulkarem.",
+        description:
+            "I graduated from Computer Systems Engineering department after 5 years of studying where i did the Full MERN Stack graduation project without any partner.",
+        icon: React.createElement(LuGraduationCap),
+        date: "June, 2022",
+    },
+    {
+        title: "Independent Contract and Freelancer",
+        location: "Remote",
+        description:
+            "I'm now a full-stack (MERN) developer working as a freelancer. Building comprehensive full stack websites for local businesses and individual clients. Delivering tailored web solutions to meet diverse needs and objectives. I'm open to full-time opportunities.",
+        icon: React.createElement(FaReact),
+        date: "2022 - present",
+    },
+    {
+        title: "Full Stack Super Visor",
+        location: "Remote | Knowledge Academy",
+        description:
+            "Supervising, providing follow-up, delivering lectures to participants, and offering guidance on appropriate solutions and best practices.",
+        icon: React.createElement(MdVilla),
+        date: "Oct, 2023 - present",
+    },
+] as const;
 
-export const projectsData = [
-	{
-		title: 'Mercy Pharm',
-		description:
-			'Developed a MERN stack website for users seeking care products. Used Node.js, Express.js, and MongoDB for backend operations. Integrated Joi for validation and Cloudinary for photo uploads. Frontend built with React.js, Vite, Tailwind CSS, Framer Motion, i18next, and Swiper for a dynamic and intuitive user experience.',
-		image: jarradMercyPharm,
-		tags: [
-			'React.js',
-			'Node.js',
-			'Express.js',
-			'MongoDB',
-			'TailwindCSS',
-			'ContextAPI',
-			'Cloudinary',
-			'i18next',
-			'Swiper',
-		],
-		imgLink: 'https://mercypharm.com',
-		github: 'https://github.com/Mohammed-Jarrad/mercypharm-frontend.git',
-		githubBackend: 'https://github.com/Mohammed-Jarrad/mercypharm-backend.git',
-		mainColor: '#3e6541',
-	},
-	{
-		title: 'Marble Net',
-		description:
-			'Crafted a marble sales platform using MERN stack. Frontend: React.js with Vite, Axios, Framer Motion, React Router DOM, SweetAlert2. Backend: Express.js, bcryptjs, JWT, Cloudinary with Multer, MongoDB with Mongoose.',
-		image: jarradMarbleNet,
-		tags: ['React.js', 'Node.js', 'Express.js', 'MongoDB', 'TailwindCSS', 'ContextAPI', 'Cloudinary'],
-		imgLink: 'https://marblenet.netlify.app',
-		github: 'https://github.com/Mohammed-Jarrad/marble-net-frontend.git',
-		githubBackend: 'https://github.com/Mohammed-Jarrad/marble-net-backend.git',
-		mainColor: '#308faf',
-	},
-	{
-		title: 'Blogs',
-		description:
-			'Designed a MERN-based blogging platform with security features, SASS styling, React-Query state management, and Axios. Boasts Cloudinary image uploads, user engagement tools, and an integrated admin dashboard for content and user oversight.',
-		image: jarradBlogs,
-		tags: ['React.js', 'Node.js', 'Express.js', 'MongoDB', 'SASS', 'React-Query'],
-		imgLink: 'https://jarrad-blogs.netlify.app',
-		github: 'https://github.com/Mohammed-Jarrad/Blogs-Frontend-V2.git',
-		githubBackend: 'https://github.com/Mohammed-Jarrad/Blogs-Backend.git',
-		mainColor: '#d25d59',
-	},
-	{
-		title: 'Social Media',
-		description:
-			'Crafted a React social media platform with SASS & swiper-js for unique stories, featuring dark mode and polished user interfaces. Highlights immersive web design using modern tools.',
-		image: jarradSocialMedia,
-		tags: ['React.js', 'SASS', 'Swiper.js'],
-		imgLink: 'https://jarrad-socialmedia.netlify.app',
-		github: 'https://github.com/Mohammed-Jarrad/social-media-app-front.git',
-		mainColor: '#5271ff',
-	},
-	{
-		title: 'Nike Showcase',
-		description:
-			'Developed a dynamic React single-page application showcasing Nike shoes, styled seamlessly with Tailwind CSS, featuring sections such as Home, Services, and Contact.',
-		image: jarradNike,
-		tags: ['React.js', 'TailwindCSS'],
-		imgLink: 'https://jarrad-nike.netlify.app',
-		github: 'https://github.com/Mohammed-Jarrad/Jarrad-Nike.git',
-		mainColor: '#ff6452',
-	},
-	{
-		title: 'Delta',
-		description:
-			'Created a security services Delta App with preset data using React.js. Styled using styled-components library, animations with framer-motion library, and sliders via react-slick library.',
-		image: jarradDelta,
-		tags: ['React.js', 'Styled-Components', 'Framer-Motion', 'React-Slick'],
-		imgLink: 'https://jarrad-delta.netlify.app',
-		github: 'https://github.com/Mohammed-Jarrad/react-delta-frontend.git',
-		mainColor: '#071c2f',
-	},
-] 
+export interface Project {
+	title: string;
+	description: string;
+	image: StaticImageData;
+	tags: string[];
+	imgLink?: string;
+	mainColor: string;
+	github?: string;
+	githubBackend?: string;
+}
+
+export const projectsData: Project[] = [
+    {
+        title: "Memo Book",
+        description:
+            "Memo Book is a web app for creating digital memory books with custom, editable and downloadable covers and pages using a Fabric.js-based editor. It supports sharing and is a fully responsive PWA with multiple languages.",
+        image: memobookImage,
+        tags: [
+            "Next.js",
+            "TypeScript",
+            "React-Query",
+            "TailwindCSS",
+            "Shadcn/UI",
+            "Framer Motion",
+            "Fabric.js",
+            "i18n",
+            "Node.js",
+            "Express.js",
+            "PostgreSQL",
+            "Cloudinary",
+        ],
+        imgLink: "https://wwww.memobook.cloud",
+        mainColor: "#7B7FEF",
+    },
+    {
+        title: "Mern Stack E-Commerce",
+        description:
+            "Developed a comprehensive e-commerce web application with full-featured user and admin functionalities.",
+        image: mernEcommerceImage,
+        tags: [
+            "React",
+            "TypeScript",
+            "React Query",
+            "TailwindCSS",
+            "Shadcn/UI",
+            "Framer Motion",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "Cloudinary",
+        ],
+        mainColor: "#7C3AED",
+        github: "https://github.com/Mohammed-Jarrad/jamela-frontend",
+        githubBackend: "https://github.com/Mohammed-Jarrad/jamela-backend",
+    },
+    {
+        title: "Mercy Pharm",
+        description:
+            "Developed a MERN stack website for users seeking care products. Used Node.js, Express.js, and MongoDB for backend operations. Integrated Joi for validation and Cloudinary for photo uploads. Frontend built with React.js, Vite, Tailwind CSS, Framer Motion, i18next, and Swiper for a dynamic and intuitive user experience.",
+        image: jarradMercyPharm,
+        tags: [
+            "React.js",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "TailwindCSS",
+            "ContextAPI",
+            "Cloudinary",
+            "i18next",
+            "Swiper",
+        ],
+        imgLink: "https://mercypharm.com",
+        github: "https://github.com/Mohammed-Jarrad/mercypharm-frontend.git",
+        githubBackend: "https://github.com/Mohammed-Jarrad/mercypharm-backend.git",
+        mainColor: "#3e6541",
+    },
+    {
+        title: "Marble Net",
+        description:
+            "Crafted a marble sales platform using MERN stack. Frontend: React.js with Vite, Axios, Framer Motion, React Router DOM, SweetAlert2. Backend: Express.js, bcryptjs, JWT, Cloudinary with Multer, MongoDB with Mongoose.",
+        image: jarradMarbleNet,
+        tags: [
+            "React.js",
+            "Node.js",
+            "Express.js",
+            "MongoDB",
+            "TailwindCSS",
+            "ContextAPI",
+            "Cloudinary",
+        ],
+        imgLink: "https://marblenet.netlify.app",
+        github: "https://github.com/Mohammed-Jarrad/marble-net-frontend.git",
+        githubBackend: "https://github.com/Mohammed-Jarrad/marble-net-backend.git",
+        mainColor: "#308faf",
+    },
+    {
+        title: "Blogs",
+        description:
+            "Designed a MERN-based blogging platform with security features, SASS styling, React-Query state management, and Axios. Boasts Cloudinary image uploads, user engagement tools, and an integrated admin dashboard for content and user oversight.",
+        image: jarradBlogs,
+        tags: ["React.js", "Node.js", "Express.js", "MongoDB", "SASS", "React-Query"],
+        imgLink: "https://jarrad-blogs.netlify.app",
+        github: "https://github.com/Mohammed-Jarrad/Blogs-Frontend-V2.git",
+        githubBackend: "https://github.com/Mohammed-Jarrad/Blogs-Backend.git",
+        mainColor: "#d25d59",
+    },
+    {
+        title: "Social Media",
+        description:
+            "Crafted a React social media platform with SASS & swiper-js for unique stories, featuring dark mode and polished user interfaces. Highlights immersive web design using modern tools.",
+        image: jarradSocialMedia,
+        tags: ["React.js", "SASS", "Swiper.js"],
+        imgLink: "https://jarrad-socialmedia.netlify.app",
+        github: "https://github.com/Mohammed-Jarrad/social-media-app-front.git",
+        mainColor: "#5271ff",
+    },
+    {
+        title: "Nike Showcase",
+        description:
+            "Developed a dynamic React single-page application showcasing Nike shoes, styled seamlessly with Tailwind CSS, featuring sections such as Home, Services, and Contact.",
+        image: jarradNike,
+        tags: ["React.js", "TailwindCSS"],
+        imgLink: "https://jarrad-nike.netlify.app",
+        github: "https://github.com/Mohammed-Jarrad/Jarrad-Nike.git",
+        mainColor: "#ff6452",
+    },
+    {
+        title: "Delta",
+        description:
+            "Created a security services Delta App with preset data using React.js. Styled using styled-components library, animations with framer-motion library, and sliders via react-slick library.",
+        image: jarradDelta,
+        tags: ["React.js", "Styled-Components", "Framer-Motion", "React-Slick"],
+        imgLink: "https://jarrad-delta.netlify.app",
+        github: "https://github.com/Mohammed-Jarrad/react-delta-frontend.git",
+        mainColor: "#071c2f",
+    },
+];
 
 export const skillsData = [
-	'HTML',
-	'CSS',
-	'TailwindCSS',
-	'SASS',
-	'JavaScript',
-	'TypeScript',
-	'React',
-	'Node.js',
-	'Express.js',
-	'MongoDB',
-	'Framer Motion',
-	'Git',
-	'GitHub',
-] as const
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "TypeScript",
+    "React",
+    "Next",
+    "React Query",
+    "TailwindCSS",
+    "SCSS",
+    "Node.js",
+    "Express.js",
+    "MongoDB",
+    "Git",
+    "GitHub",
+] as const;
